594397285750a3c8392755a9bab13b14a64e0587
Julien L <julienlenaz96@gmail.com>
2023-08-30 19:48:00 -0400

    Added get/set methods

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -5,41 +5,26 @@ namespace ShapeTracker.Models
   public class Triangle 
   {
     private int _side1;
-    private int _side2;
+    public int Side1
+    {
+      get { return _side1; }
+      set { _side1 = value; }
+    }
+    public int Side2 { get; set; }
     private int _side3;
     private static List<Triangle> _instances = new List<Triangle> {};
 
     public Triangle(int length1, int length2, int length3) 
     {
       _side1 = length1;
-      _side2 = length2;
+      Side2 = length2;
       _side3 = length3;
       _instances.Add(this);
-    }
-
-    public int GetSide1()
-    {
-      return _side1;
-    }
-
-    public int GetSide2()
-    {
-      return _side2;
     }
 
     public int GetSide3()
     {
       return _side3;
-    }
-
-    public void SetSide1(int newSide)
-    {
-      _side1 = newSide;
-    }
-
-    public void SetSide2(int newSide)
-    {
-      _side2 = newSide;
     }
 
     public void SetSide3(int newSide)
@@ -49,15 +34,15 @@ namespace ShapeTracker.Models
 
     public string CheckType() 
     {
-      if ((_side1 > (_side2 + _side3)) || (_side2 > (_side1 + _side3)) || (_side3 > (_side1 + _side2)))
+      if ((_side1 > (Side2 + _side3)) || (Side2 > (_side1 + _side3)) || (_side3 > (_side1 + Side2)))
       {
         return "not a triangle";
       } 
-      else if ((_side1 != _side2) && ((_side1 != _side3)) && ((_side2 != _side3))) 
+      else if ((_side1 != Side2) && ((_side1 != _side3)) && ((Side2 != _side3))) 
       {
         return "scalene triangle";
       }  
-      else if ((_side1 == _side2) && (_side1 == _side3)) 
+      else if ((_side1 == Side2) && (_side1 == _side3)) 
       {
         return "equilateral triangle";
       } 
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -26,8 +26,8 @@ namespace ShapeTracker
     static void ConfirmOrEditTriangle(Triangle tri)
     {
       Console.WriteLine("Please confirm that you entered in your triangle correctly:");
-      Console.WriteLine($"Side 1 has a length of {tri.GetSide1()}.");
-      Console.WriteLine($"Side 2 has a length of {tri.GetSide2()}.");
+      Console.WriteLine($"Side 1 has a length of {tri.Side1}.");
+      Console.WriteLine($"Side 2 has a length of {tri.Side2}.");
       Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
       Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides");
       string userInput = Console.ReadLine();  
@@ -44,8 +44,8 @@ namespace ShapeTracker
         string stringNumber2 = Console.ReadLine();  
         Console.WriteLine("Enter a third number:");
         string stringNumber3 = Console.ReadLine();  
-        tri.SetSide1(int.Parse(stringNumber1));  
-        tri.SetSide2(int.Parse(stringNumber2));  
+        tri.Side1 = int.Parse(stringNumber1);  
+        tri.Side2 = int.Parse(stringNumber2);  
         tri.SetSide3(int.Parse(stringNumber3)); 
         ConfirmOrEditTriangle(tri);
       }
