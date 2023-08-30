84080b4f793c0dfe13668a48254814367c5571d9
Julien L <julienlenaz96@gmail.com>
2023-08-29 22:51:48 -0400

    Added new classes

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,7 +1,13 @@
-namespace ShapeTracker.Models
-{
-  public class Triangle
-  {
+using System;
 
+namespace ShapeTracker
+{
+  class Program
+  {
+    static void Main()
+    {
+      ShapeTracker.Models.Triangle testTriangle = new ShapeTracker.Models.Triangle();
+      Console.WriteLine(testTriangle.GetType());
+    }
   }
 }
