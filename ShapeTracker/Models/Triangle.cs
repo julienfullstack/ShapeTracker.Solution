d40e1509aed054f3fed7df4167f08cbdc6ead81c
Julien L <julienlenaz96@gmail.com>
2023-08-30 23:55:34 -0400

    added function to CheckType to check if the triangle is a scalene triangle, as well as testing

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -27,9 +27,20 @@ namespace ShapeTracker.Models
     {
       _side3 = newValue;
     }
-    public string CheckType()
+  public string CheckType()
     {
-      return "not a triangle";
+      if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2))) 
+      {
+        return "not a triangle";
+      } 
+      else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3))) 
+      {
+        return "scalene triangle";
+      }
+      else
+      {
+        return "";
+      }
     }
   }
 }
