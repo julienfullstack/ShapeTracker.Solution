c0d0d1f613583b72b533f3a31370015411c770a4
Julien L <julienlenaz96@gmail.com>
2023-09-05 16:34:04 -0400

    Created and passed isosceles checktype method

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -106,5 +106,16 @@ namespace ShapeTracker.Tests
       // Assert
       Assert.AreEqual("scalene triangle", triType);
     }
+    
+   [TestMethod]
+    public void CheckType_DeterminesWhenIsosceles_String()
+    {
+      // Arrange
+      Triangle isoscelesTri = new Triangle(5, 4, 4);
+      // Act
+      string triType = isoscelesTri.CheckType();
+      // Assert
+      Assert.AreEqual("isosceles triangle", triType);
+    }
   }
 }
