010048464522a38e1e84f14c061f32a9afa3a7b6
Julien L <julienlenaz96@gmail.com>
2023-09-05 16:38:15 -0400

    Created and pass test for checktype method for equilaterial triangle

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -116,6 +116,17 @@ namespace ShapeTracker.Tests
       string triType = isoscelesTri.CheckType();
       // Assert
       Assert.AreEqual("isosceles triangle", triType);
+    
+    }
+     [TestMethod]
+    public void CheckType_DeterminesWhenEquilateral_String()
+    {
+      // Arrange
+      Triangle equiTri = new Triangle(4, 4, 4);
+      // Act
+      string triType = equiTri.CheckType();
+      // Assert
+      Assert.AreEqual("equilateral triangle", triType);
     }
   }
 }
